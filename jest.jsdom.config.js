35ab081976bffef97eb7621ee7ed14557c33f7cb
sooraj.s <sooraj.s@ibilglobal.com>
2021-10-02 18:24:50 +0530

    next init

--- jest.jsdom.config.js
+++
@@ -1,8 +0,0 @@
-const baseConfig = require('./jest.base.config');
-
-module.exports = {
-  ...baseConfig,
-  testEnvironment: 'jsdom',
-  testMatch: ['**/*.test.tsx'],
-  setupTestFrameworkScriptFile: './setupTests.ts',
-};
--- jest.node.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require('./jest.base.config');
-
-module.exports = {
-  ...baseConfig,
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-};
